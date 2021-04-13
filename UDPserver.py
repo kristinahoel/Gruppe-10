88a51c0f383d7850c29b88c5e26a367371620cf2
kjellhs <kjell.herman.solberg@gmail.com>
2021-04-13 11:51:26 +0200

    Ferdig versjon av oppgave3.26

--- UDPserver.py
+++ UDPserver.py
@@ -1,24 +1,23 @@
 __author__ = 'jiayingyu'
 # UDPPingerServer.py
-# We will need the following module to generate randomized lost packets
+# Moduler:
 import random
 from socket import *
 
-# Create a UDP socket
-# Notice the use of SOCK_DGRAM for UDP packets
+# Lager socketen
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-# Assign IP address and port number to socket
+# Setter IP-adresse og port-nummer
 serverSocket.bind(('', 12000))
 print("Started UDP server on port 12000")
 while True:
-    # Generate random number in the range of 0 to 10
+    # Generer et tilfeldig tall mellom 0 og 10
     rand = random.randint(0, 10)
-    # Receive the client packet along with the address it is coming from
+    # Mottar meldingen fra klienten
     message, address = serverSocket.recvfrom(1024)
-    # Capitalize the message from the client
+    # Gjør meldingen den mottar til stor skrift
     message = message.upper()
-    # If rand is less is than 4, we consider the packet lost and do not respond
+    # Hvis rand er mindre enn 4, anser vi meldingen som tapt, og det vil time ut
     if rand < 4:
         continue
-    # Otherwise, the server responds
+    # Hvis ikke, svarer serveren
     serverSocket.sendto(message, address)
--- oppgave326.c
+++ oppgave326.c
@@ -1,6 +1,34 @@
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+
+int main(int argc, char** argv){
+    int fd[2];
+    char message[10] = "Hi there!\n";
+    if (pipe(fd) == -1) {
+        printf("There was an error in pipe creation. Program will now exit.\n");
+        return -1;
+    }
+    pid_t child;
+    child = fork();
+    if(child < 0) {
+        printf("Forking failed.");
+        return -1;
+    }
+    if(child == 0) {
+        close(fd[0]);
+        switchCases(message);
+        write(fd[1],message,10);
+    }
+    else if(child > 0) {
+        close(fd[1]);
+        char read_buffer[10];
+        read(fd[0],read_buffer,10);
+        printf("%s",read_buffer);
+    }
+    return 0;
+}
 
 void switchCases(char* message) {
     int i = 0;
@@ -13,30 +41,4 @@ void switchCases(char* message) {
         }
         i++;
     }
-int main(int argc, char** argv){
-    int fd[2];
-    char message[10] = "Hi there!";
-    if (pipe(fd) == -1) {
-        printf("There was an error in pipe creation. Program will now exit.\n");
-        return -1;
-    }
-    pid_t child;
-    child = fork();
-    if(child < 0) {
-        printf("Forking failed.\n");
-        return -1;
-    }
-    if(child == 0) {
-        close(fd[0]);
-        switchCases(message);
-        write(fd[1],message,10);
-    }
-    else if(child > 0) {
-        close(fd[1]);
-        char read_buffer[10];
-        read(fd[0],read_buffer,10);
-        printf("%s \n",read_buffer);
-    }
-    return 0;
-}
 }
