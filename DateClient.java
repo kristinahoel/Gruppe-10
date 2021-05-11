b65bd7e35953005ef7a5972f11d8dcbee393b287
Henrik <henriksj@uia.no>
2021-05-11 18:34:42 +0200

    Oppgave323

---
+++ DateClient.java
@@ -0,0 +1,31 @@
+import java.net.*;
+import java.io.*;
+
+public class DateClient
+{
+        public static void main(String[] args) {
+         try {
+                /* make connection to server socket */
+                Socket sock = new Socket("127.0.1.1", 6017);
+
+                InputStream in = sock.getInputStream();
+                /* calling buffer reader object */
+                  BufferedReader bin = new
+                         BufferedReader (new InputStreamReader( in ));
+
+
+                /* read the date from the socket */
+                String line;
+                while( (line = bin.readLine()) != null)
+                  System.out.println(line);
+
+                /* close the socket connection */
+                sock.close();
+               }
+               catch (IOException ioe) {
+                System.err.println(ioe);
+               }
+            }
+        }
+
+
